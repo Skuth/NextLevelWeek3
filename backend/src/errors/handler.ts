862bda675a736a0e3b9b7bf6ac3c64b9c37b88c7
SKUTH <flavinhoyt@gmail.com>
2021-01-19 09:31:28 -0300

    Adicionando README

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,13 +1,13 @@
 import { ErrorRequestHandler } from "express"
 import { ValidationError } from "yup"
 
-interface ValidationErrors {
+interface validationErrors {
   [Key: string]: string[]
 }
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   if (error instanceof ValidationError) {
-    let errors: ValidationErrors = {}
+    let errors: validationErrors = {}
 
     error.inner.forEach(err => {
       errors[err.path] = err.errors
